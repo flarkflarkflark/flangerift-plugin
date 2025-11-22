2f70d0e5dae4887ea4ebf716fdd4a96de3783970
flarkflarkflark <69244166+flarkflarkflark@users.noreply.github.com>
2025-11-22 04:59:01 +0100

    Add FlangeriftEditor class for audio processing

---
+++ src/PluginEditor.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+class FlangeriftEditor : public juce::AudioProcessorEditor
+{
+public:
+    FlangeriftEditor(FlangeriftProcessor&);
+    ~FlangeriftEditor() override;
+
+    void paint(juce::Graphics&) override;
+    void resized() override;
+
+private:
+    FlangeriftProcessor& audioProcessor;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FlangeriftEditor)
+};
